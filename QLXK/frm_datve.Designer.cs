260ec33b4456636a9a1130edbfe12857e79cf909
jimmyhauu1510 <144302927+jimmyhauu1510@users.noreply.github.com>
2023-09-23 18:55:53 +0700

    Add files via upload

---
+++ QLXK/frm_datve.Designer.cs
@@ -0,0 +1,333 @@
+﻿namespace QLXK
+{
+    partial class frm_datve
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.dtp_ngayden = new System.Windows.Forms.DateTimePicker();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lbl_sove = new System.Windows.Forms.Label();
+            this.lbl_ngayden = new System.Windows.Forms.Label();
+            this.lbl_chonden = new System.Windows.Forms.Label();
+            this.lbl_chondi = new System.Windows.Forms.Label();
+            this.btn_datve = new System.Windows.Forms.Button();
+            this.linklbl_datve = new System.Windows.Forms.LinkLabel();
+            this.cb_sove = new System.Windows.Forms.ComboBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.rdb_Khuhoi = new System.Windows.Forms.RadioButton();
+            this.rdb_1chieu = new System.Windows.Forms.RadioButton();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.linhlbl_QR = new System.Windows.Forms.LinkLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pic_poster = new System.Windows.Forms.PictureBox();
+            this.pic_Swap = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_poster)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Swap)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.dateTimePicker1);
+            this.panel1.Controls.Add(this.dtp_ngayden);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.lbl_sove);
+            this.panel1.Controls.Add(this.lbl_ngayden);
+            this.panel1.Controls.Add(this.lbl_chonden);
+            this.panel1.Controls.Add(this.lbl_chondi);
+            this.panel1.Controls.Add(this.btn_datve);
+            this.panel1.Controls.Add(this.linklbl_datve);
+            this.panel1.Controls.Add(this.cb_sove);
+            this.panel1.Controls.Add(this.pic_Swap);
+            this.panel1.Controls.Add(this.textBox2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.rdb_Khuhoi);
+            this.panel1.Controls.Add(this.rdb_1chieu);
+            this.panel1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.panel1.Location = new System.Drawing.Point(12, 359);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1105, 229);
+            this.panel1.TabIndex = 0;
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Font = new System.Drawing.Font("#9Slide03 Open Sans", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dateTimePicker1.Location = new System.Drawing.Point(559, 84);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(283, 25);
+            this.dateTimePicker1.TabIndex = 7;
+            // 
+            // dtp_ngayden
+            // 
+            this.dtp_ngayden.Font = new System.Drawing.Font("#9Slide03 Open Sans", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.dtp_ngayden.Location = new System.Drawing.Point(559, 147);
+            this.dtp_ngayden.Name = "dtp_ngayden";
+            this.dtp_ngayden.Size = new System.Drawing.Size(283, 25);
+            this.dtp_ngayden.TabIndex = 7;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("#9Slide03 Open Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(555, 53);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 24);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Ngày đi";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl_sove
+            // 
+            this.lbl_sove.AutoSize = true;
+            this.lbl_sove.Font = new System.Drawing.Font("#9Slide03 Open Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_sove.Location = new System.Drawing.Point(912, 90);
+            this.lbl_sove.Name = "lbl_sove";
+            this.lbl_sove.Size = new System.Drawing.Size(52, 24);
+            this.lbl_sove.TabIndex = 6;
+            this.lbl_sove.Text = "Số vé";
+            this.lbl_sove.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl_ngayden
+            // 
+            this.lbl_ngayden.AutoSize = true;
+            this.lbl_ngayden.Font = new System.Drawing.Font("#9Slide03 Open Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_ngayden.Location = new System.Drawing.Point(555, 116);
+            this.lbl_ngayden.Name = "lbl_ngayden";
+            this.lbl_ngayden.Size = new System.Drawing.Size(84, 24);
+            this.lbl_ngayden.TabIndex = 6;
+            this.lbl_ngayden.Text = "Ngày đến";
+            this.lbl_ngayden.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl_chonden
+            // 
+            this.lbl_chonden.AutoSize = true;
+            this.lbl_chonden.Font = new System.Drawing.Font("#9Slide03 Open Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_chonden.Location = new System.Drawing.Point(293, 90);
+            this.lbl_chonden.Name = "lbl_chonden";
+            this.lbl_chonden.Size = new System.Drawing.Size(129, 24);
+            this.lbl_chonden.TabIndex = 6;
+            this.lbl_chonden.Text = "Chọn điểm đến";
+            this.lbl_chonden.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lbl_chondi
+            // 
+            this.lbl_chondi.AutoSize = true;
+            this.lbl_chondi.Font = new System.Drawing.Font("#9Slide03 Open Sans", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_chondi.Location = new System.Drawing.Point(18, 90);
+            this.lbl_chondi.Name = "lbl_chondi";
+            this.lbl_chondi.Size = new System.Drawing.Size(113, 24);
+            this.lbl_chondi.TabIndex = 6;
+            this.lbl_chondi.Text = "Chọn điểm đi";
+            this.lbl_chondi.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // btn_datve
+            // 
+            this.btn_datve.BackColor = System.Drawing.Color.Khaki;
+            this.btn_datve.Font = new System.Drawing.Font("#9Slide03 Nexa Light", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_datve.Location = new System.Drawing.Point(435, 178);
+            this.btn_datve.Name = "btn_datve";
+            this.btn_datve.Size = new System.Drawing.Size(188, 51);
+            this.btn_datve.TabIndex = 5;
+            this.btn_datve.Text = "ĐẶT VÉ NGAY";
+            this.btn_datve.UseVisualStyleBackColor = false;
+            this.btn_datve.Click += new System.EventHandler(this.btn_datve_Click);
+            // 
+            // linklbl_datve
+            // 
+            this.linklbl_datve.AutoSize = true;
+            this.linklbl_datve.Font = new System.Drawing.Font("#9Slide03 Open Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linklbl_datve.Location = new System.Drawing.Point(868, 16);
+            this.linklbl_datve.Name = "linklbl_datve";
+            this.linklbl_datve.Size = new System.Drawing.Size(207, 28);
+            this.linklbl_datve.TabIndex = 4;
+            this.linklbl_datve.TabStop = true;
+            this.linklbl_datve.Text = "HƯỚNG DẪN ĐẶT VÉ";
+            // 
+            // cb_sove
+            // 
+            this.cb_sove.FormattingEnabled = true;
+            this.cb_sove.Items.AddRange(new object[] {
+            "1",
+            "2",
+            "3",
+            "4",
+            "5",
+            "6",
+            "7",
+            "8",
+            "9",
+            "10"});
+            this.cb_sove.Location = new System.Drawing.Point(916, 125);
+            this.cb_sove.Name = "cb_sove";
+            this.cb_sove.Size = new System.Drawing.Size(159, 24);
+            this.cb_sove.TabIndex = 3;
+            this.cb_sove.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(297, 127);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(193, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(14, 127);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(193, 22);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // rdb_Khuhoi
+            // 
+            this.rdb_Khuhoi.AutoSize = true;
+            this.rdb_Khuhoi.Font = new System.Drawing.Font("#9Slide03 Open Sans", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdb_Khuhoi.Location = new System.Drawing.Point(155, 16);
+            this.rdb_Khuhoi.Name = "rdb_Khuhoi";
+            this.rdb_Khuhoi.Size = new System.Drawing.Size(101, 30);
+            this.rdb_Khuhoi.TabIndex = 0;
+            this.rdb_Khuhoi.TabStop = true;
+            this.rdb_Khuhoi.Text = "Khứ Hồi";
+            this.rdb_Khuhoi.UseVisualStyleBackColor = true;
+            this.rdb_Khuhoi.CheckedChanged += new System.EventHandler(this.rdb_Khuhoi_CheckedChanged);
+            // 
+            // rdb_1chieu
+            // 
+            this.rdb_1chieu.AutoSize = true;
+            this.rdb_1chieu.Font = new System.Drawing.Font("#9Slide03 Open Sans", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdb_1chieu.Location = new System.Drawing.Point(14, 16);
+            this.rdb_1chieu.Name = "rdb_1chieu";
+            this.rdb_1chieu.Size = new System.Drawing.Size(117, 30);
+            this.rdb_1chieu.TabIndex = 0;
+            this.rdb_1chieu.TabStop = true;
+            this.rdb_1chieu.Text = "Một chiều";
+            this.rdb_1chieu.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.pictureBox1);
+            this.panel2.Controls.Add(this.linhlbl_QR);
+            this.panel2.Controls.Add(this.pic_poster);
+            this.panel2.Location = new System.Drawing.Point(12, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(1105, 329);
+            this.panel2.TabIndex = 0;
+            // 
+            // linhlbl_QR
+            // 
+            this.linhlbl_QR.AutoSize = true;
+            this.linhlbl_QR.BackColor = System.Drawing.Color.White;
+            this.linhlbl_QR.Font = new System.Drawing.Font("#9Slide03 Open Sans", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linhlbl_QR.LinkColor = System.Drawing.Color.Red;
+            this.linhlbl_QR.Location = new System.Drawing.Point(850, 282);
+            this.linhlbl_QR.Name = "linhlbl_QR";
+            this.linhlbl_QR.Size = new System.Drawing.Size(222, 26);
+            this.linhlbl_QR.TabIndex = 4;
+            this.linhlbl_QR.TabStop = true;
+            this.linhlbl_QR.Text = "Quét mã để đặt vé ngay !";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::QLXK.Properties.Resources.QR;
+            this.pictureBox1.Location = new System.Drawing.Point(951, 165);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(124, 114);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pic_poster
+            // 
+            this.pic_poster.Image = global::QLXK.Properties.Resources.Untitled;
+            this.pic_poster.Location = new System.Drawing.Point(6, 6);
+            this.pic_poster.Name = "pic_poster";
+            this.pic_poster.Size = new System.Drawing.Size(1099, 323);
+            this.pic_poster.TabIndex = 5;
+            this.pic_poster.TabStop = false;
+            // 
+            // pic_Swap
+            // 
+            this.pic_Swap.Image = global::QLXK.Properties.Resources.sort;
+            this.pic_Swap.Location = new System.Drawing.Point(229, 113);
+            this.pic_Swap.Name = "pic_Swap";
+            this.pic_Swap.Size = new System.Drawing.Size(46, 46);
+            this.pic_Swap.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pic_Swap.TabIndex = 2;
+            this.pic_Swap.TabStop = false;
+            // 
+            // frm_datve
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1129, 620);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "frm_datve";
+            this.Text = "Ampersand Travel";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_poster)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pic_Swap)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RadioButton rdb_Khuhoi;
+        private System.Windows.Forms.RadioButton rdb_1chieu;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pic_Swap;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button btn_datve;
+        private System.Windows.Forms.LinkLabel linklbl_datve;
+        private System.Windows.Forms.ComboBox cb_sove;
+        private System.Windows.Forms.Label lbl_chondi;
+        private System.Windows.Forms.Label lbl_chonden;
+        private System.Windows.Forms.Label lbl_sove;
+        private System.Windows.Forms.Label lbl_ngayden;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.DateTimePicker dtp_ngayden;
+        private System.Windows.Forms.LinkLabel linhlbl_QR;
+        private System.Windows.Forms.PictureBox pic_poster;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ QLXK/frm_datve.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLXK
+{
+    public partial class frm_datve : Form
+    {
+        public frm_datve()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void rdb_Khuhoi_CheckedChanged(object sender, EventArgs e)
+        {
+            if(rdb_1chieu.Checked == true)
+            {
+                lbl_ngayden.Visible = false;
+                dtp_ngayden.Visible = false;
+            }
+            else
+            {
+                lbl_ngayden.Width = 0;
+                lbl_ngayden.Visible = true;
+                dtp_ngayden.Visible = true;
+            }
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            rdb_Khuhoi.Checked = true;
+            cb_sove.SelectedItem = "1";
+           // linhlbl_QR.BackColor = System.Drawing.Color.Transparent;
+
+        }
+
+        private void btn_datve_Click(object sender, EventArgs e)
+        {
+          
+        }
+    }
+}
